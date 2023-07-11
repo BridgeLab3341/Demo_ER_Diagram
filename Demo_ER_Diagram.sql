486ad7663b5192206e958ee1a5d71c6a0c1fb51f
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-07-11 13:42:13 +0530

    [somu]Add:Creating Tables to Create ER Diagram Usig Sql.

---
+++ Demo_ER_Diagram.sql
@@ -0,0 +1,19 @@
+Create Database Demo_ER_Diagram
+
+--1. Create COUNTRY Table
+Create Table COUNTRY(CountryId Int Identity Primary Key,
+CountryName Varchar(30),Continent Varchar(10),Currency Varchar(3));
+
+--2.Create DEPARTMENT Table
+Create Table DEPARTMENT(DepartmentId Int Primary Key,
+DeptName Varchar(30),CountryId Int Foreign Key	References dbo.COUNTRY(CountryId));
+
+--3.Create EMPLOYEE Table
+Create Table EMPLOYEE(EmployeeId Int Primary Key,
+EmpName Varchar(20) NOT NULL,
+DeptId Int Foreign Key References dbo.DEPARTMENT(DepartmentId)NULL);
+
+--4.Create FOLDER Table
+Create Table FOLDER(FolderId Int NOT NULL,
+EmployeeId Int References EMPLOYEE(EmployeeId),
+AccessType varchar(5) NULL);
